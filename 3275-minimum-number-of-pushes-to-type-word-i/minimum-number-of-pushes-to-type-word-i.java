d2825ae8575b5e9af0a590d21363c45478a15feb
Archit Srivastava <149877302+architSrivastava123@users.noreply.github.com>
2026-07-30 12:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (89.00%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int minimumPushes(String word) {
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 22, 24,27, 30, 33, 36, 39, 42, 45, 48,52, 56};
+    return arr[word.length()];
+    
+    }
+}
